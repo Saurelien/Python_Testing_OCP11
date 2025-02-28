cc3e7374b3f4949719806087a6f29d8c8c05a36f
Saurelien <aurelienschmitt.info@gmail.com>
2025-02-28 11:43:42 +0100

    Added some pep8 structuration

--- test/test_unit/test_unit.py
+++ test/test_unit/test_unit.py
@@ -1,6 +1,4 @@
 from datetime import datetime, timedelta
-from http.client import responses
-
 from test import mocks
 from server import get_competition_by_name, get_club_by_name
 
@@ -26,42 +24,20 @@ def test_get_club_error(mock_clubs):
     club = get_club_by_name("random_email.com")
     assert club is None
 
+
 def test_index_page(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b"GUDLFT" in response.data
+
 
 def test_show_summary_valid_email(client, mock_clubs):
     response = client.post('/showSummary', data={'email': mocks.VALID_EMAIL})
     assert response.status_code == 200
     assert b"Welcome" in response.data
 
+
 def test_show_summary_invalid_email(client, mock_clubs):
     response = client.post('/showSummary', data={'email': 'fake@email.com'})
     assert response.status_code == 200
     assert b"Email non valide ou non existant" in response.data
-
-
-def test_book(client, mock_clubs, mock_competitions):
-    future_competition = {
-        'name': 'Future Competition',
-        'date': (datetime.now() + timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
-        'numberOfPlaces': 10
-    }
-    mocks.MOCK_COMPETITIONS.append(future_competition)
-
-    response = client.get(f'/book/{future_competition["name"]}/{mocks.CLUB_VALID}')
-    assert response.status_code == 200
-    assert 'How many places?' in response.data.decode('utf-8')
-
-    past_competition = {
-        'name': 'Past Competition',
-        'date': (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S'),
-        'numberOfPlaces': 5
-    }
-    mocks.MOCK_COMPETITIONS.append(past_competition)
-
-    response = client.get(f'/book/{past_competition["name"]}/{mocks.CLUB_VALID}')
-    assert response.status_code == 200
-    assert 'La compétition a déjà eu lieu' in response.data.decode('utf-8')
-    assert 'How many places?' not in response.data.decode('utf-8')
